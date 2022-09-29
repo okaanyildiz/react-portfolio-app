910776b52ded992922446ca59d5dde7788c703bb
Ozi <oguzkaanyildiz@mac-ozi.local>
2022-09-29 14:47:22 +0200

    Make Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import MailchimpForm from "./MailchimpForm";
+
+function Footer() {
+    return (
+        <footer className="footer" id="footer">
+            <MailchimpForm />
+            <p>Copyright 2022. All Rights Reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
