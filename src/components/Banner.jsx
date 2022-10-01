5876e4b88cd9f0d15cd4ade00a2685f7ff0ecc41
Ozi <oguzkaanyildiz@mac-ozi.local>
2022-10-01 15:04:13 +0200

    Change whole background style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
 import profilImg from "../assets/img/profil-img.jpg";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
 import { HashLink } from 'react-router-hash-link';
 import {
     BrowserRouter as Router
@@ -15,26 +13,20 @@ function Banner() {
                 <Container>
                     <Row className="align-items-center">
                         <Col xs={12} md={6} xl={7}>
-                            <TrackVisibility>
-                                {({ isVisible }) =>
-                                    <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                                        <span className="tagline">Welcome!</span>
-                                        <h1>I'm Oguz</h1>
-                                        {/* <h2>Software Developer</h2> */}
-                                        <p>With years of experience, passionate about developing and collaborating. Always eager to learn new technologies.</p>
-                                        <HashLink to='#footer'>
-                                            <button onClick={() => console.log('connect')}>Let’s Connect</button>
-                                        </HashLink>
-                                    </div>}
-                            </TrackVisibility>
+                            <div>
+                                <span className="tagline">Welcome!</span>
+                                <h1>I'm Oguz</h1>
+                                <h2>Software Developer</h2>
+                                <p>With years of experience, passionate about developing and collaborating. Always eager to learn new technologies.</p>
+                                <HashLink to='#footer'>
+                                    <button onClick={() => console.log('connect')}>Let’s Connect</button>
+                                </HashLink>
+                            </div>
                         </Col>
                         <Col xs={12} md={4} xl={5}>
-                            <TrackVisibility>
-                                {({ isVisible }) =>
-                                    <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                                        <img src={profilImg} alt="Header Img" />
-                                    </div>}
-                            </TrackVisibility>
+                            <div>
+                                <img src={profilImg} alt="Header Img" />
+                            </div>
                         </Col>
                     </Row>
                 </Container>
