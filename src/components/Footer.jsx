0f839422255ec169cf20ba268c7f706dba05ef5e
Ozi <oguzkaanyildiz@mac-ozi.local>
2022-10-04 15:48:26 +0200

    Renew the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,20 @@
-import MailchimpForm from "./MailchimpForm";
+import linkedin from '../assets/img/nav-icon-linkedin.svg';
+import email from "../assets/img/nav-icon-email.svg"
 
 function Footer() {
     return (
         <footer className="footer" id="footer">
-            <MailchimpForm />
+            <h1>Let's Get in Touch!</h1>
+            <div className="connect-icon-box">
+                <a href="https://www.linkedin.com/in/oguzkaanyildiz/" target="_blank" rel="noreferrer">
+                    <img src={linkedin} alt="linkedin" />
+                </a>
+                <a href="mailto:okaanyildiz@yahoo.com">
+                    <img src={email} alt="email" />
+                </a>
+            </div>
             <p>Copyright 2022. All Rights Reserved</p>
-        </footer>
+        </footer >
     )
 }
 
